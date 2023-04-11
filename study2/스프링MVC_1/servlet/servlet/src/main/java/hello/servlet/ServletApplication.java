07c15e0f0667e351677d0f2d884bf9ec3508a146
ryujh_mac <ryujh8891@gmail.com>
2023-04-11 22:46:30 +0900

    2023.04.11 - 뷰리졸버까지 공부 완료

--- study2/스프링MVC_1/servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
+++ study2/스프링MVC_1/servlet/servlet/src/main/java/hello/servlet/ServletApplication.java
@@ -3,6 +3,8 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan //서블릿 자동 등록
 @SpringBootApplication
@@ -12,5 +14,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	@Bean
+	InternalResourceViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("/WEB-INF/views/", ".jsp");
+	}
 }
 
